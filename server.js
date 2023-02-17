c280ecf11162f50c10fdacdd0e3e38c8d8b7d1d3
= <astakhov00@gmail.com>
2023-02-16 23:57:26 -0500

    test

--- server.js
+++ server.js
@@ -22,10 +22,13 @@ const reservationsRouter = require("./routes/reservations");
 // Connect to MongoDB
 mongoose.set("strictQuery", true);
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    "mongodb+srv://admin:wwwwww@cluster0.wbhqsfn.mongodb.net/combike?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => {
     console.log("MongoDB connected...");
   })
