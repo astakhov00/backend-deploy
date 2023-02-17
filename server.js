2c2437d1194aa9252c0b3e812e41c390e4013f31
= <astakhov00@gmail.com>
2023-02-17 08:36:36 -0500

    added cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const jwt = require("jsonwebtoken");
+const cors = require("cors");
 
 const errorHandler = require("./utils/errorHandler");
 
@@ -9,6 +10,7 @@ dotenv.config();
 
 const app = express();
 
+app.use(cors());
 // Routes
 const locationsRouter = require("./routes/locations");
 app.use("/locations", locationsRouter);
